f91a51d740f2c8fc531ddbbc5a0df3545c527ee3
202310957 <snu11025@naver.com>
2026-03-30 18:27:29 +0900

    [Refactor] 중복되는 파일 제거

--- src/service/weather_service.py
+++
@@ -1,7 +0,0 @@
-from src.client.weather import get_environment_info
-
-def get_weather_info(lat: float, lng: float):
-    """
-    client 레이어 호출 (비즈니스 로직 확장 가능)
-    """
-    return get_environment_info(lat, lng)
