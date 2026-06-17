8f0cdb207bb988b06bd7a4a0289ed0b683f4bf64
202310957 <snu11025@naver.com>
2026-06-18 04:37:28 +0900

    [Refactor] 바뀐 walk_schema.py에 따라 수정

--- frontend/streamlit_prototype/modes/circular.py
+++ frontend/streamlit_prototype/modes/circular.py
@@ -9,4 +9,4 @@ from frontend.streamlit_prototype.modes.base import BaseRouteMode
 
 class CircularMode(BaseRouteMode):
     label    = "순환 산책 (제자리 돌아오기)"
-    mode_key = "circular"
+    mode_key = "circular_random"
