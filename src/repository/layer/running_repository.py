ab1e00effcb78711b40f3526f01cbe964c36fbba
202310957 <snu11025@naver.com>
2026-06-19 15:20:57 +0900

    [Merge] dev와 충돌 해결

--- src/repository/layer/running_repository.py
+++ src/repository/layer/running_repository.py
@@ -13,7 +13,6 @@ from sqlalchemy import func, insert, select
 
 from src.database.postgresql import get_postgresql_db
 from src.entity.layer.running_layer import RunningLayer
-from src.interfaces.schema.running_schema import CourseInfo
 from src.repository.utils import RepositoryUtils
 
 
