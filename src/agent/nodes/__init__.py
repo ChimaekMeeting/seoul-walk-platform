d6b22cdbf1aa53ca6ec41685a90537827874fe16
202310957 <snu11025@naver.com>
2026-09-23 03:08:17 +0900

    [Delete] confirmation_classifier 노드 제거

--- src/agent/nodes/__init__.py
+++ src/agent/nodes/__init__.py
@@ -1,5 +1,4 @@
 from src.agent.nodes.extractor import Extractor
 from src.agent.nodes.weight_extractor import WeightExtractor
 from src.agent.nodes.interviewer import Interviewer
-from src.agent.nodes.confirmation_classifier import ConfirmationClassifier
 from src.agent.nodes.route_executor import RouteExecutor
